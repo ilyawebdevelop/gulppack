bd04dccb6eeea06adac4368da2aa7f53176d0808
ilyawebdevelop <ilia966@tut.by>
2022-03-04 23:51:15 +0300

    My Gulp -first

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import * as flsFunctions from "./modules/functions.js";
+import vars from './vars';
+import './modules';
+import './modules/functions';
+import './components';
+
+flsFunctions.isWebp();
+
+/*
+import Swiper, { Navigation, Pagination } from 'swiper';
+const swiper = new Swiper();
+*/
